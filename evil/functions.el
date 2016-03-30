44b18894f8cfa554c05a47c191792f628140e84b
DaVinci789 <jaden.manacsa1228@gmail.com>
2016-03-30 11:08:03 -0500

    add evil functions

---
+++ evil/functions.el
@@ -0,0 +1,33 @@
+;;; functions -- functions for Emacs' Evil mode
+;;; Commentary:
+;; Changes:
+;; ------------
+;;
+;;; Code:
+(load-file "packages.el")
+
+;; define movement macro
+(defmacro define-and-bind-text-object (key start-regex end-regex)
+  "Create new evil text object based on KEY, START-REGEX, and END-REGEX."
+  (let ((inner-name (make-symbol "inner-name"))
+        (outer-name (make-symbol "outer-name")))
+    `(progn
+       (evil-define-text-object ,inner-name (count &optional beg end type)
+         (evil-select-paren ,start-regex ,end-regex beg end type count nil))
+       (evil-define-text-object ,outer-name (count &optional beg end type)
+         (evil-select-paren ,start-regex ,end-regex beg end type count t))
+       (define-key evil-inner-text-objects-map ,key (quote ,inner-name))
+       (define-key evil-outer-text-objects-map ,key (quote ,outer-name)))))
+
+(defun minibuffer-keyboard-quit ()
+  "Abort recursive edit.
+In Delete Selection mode, if the mark is active, just deactivate it;
+then it takes a second \\[keyboard-quit] to abort the minibuffer."
+  (interactive)
+  (if (and delete-selection-mode transient-mark-mode mark-active)
+      (setq deactivate-mark  t)
+    (when (get-buffer "*Completions*") (delete-windows-on "*Completions*"))
+    (abort-recursive-edit)))
+
+(provide 'functions)
+;;; functions.el ends here
